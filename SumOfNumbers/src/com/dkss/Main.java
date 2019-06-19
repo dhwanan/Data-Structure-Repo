0f094555317e0db361af5fd35e821e48f06ce16a
Dhwanan Shelat <dkshelat17@gmail.com>
2019-06-18 23:24:27 -0700

    Sum of first 17 numbers

---
+++ SumOfNumbers/src/com/dkss/Main.java
@@ -0,0 +1,18 @@
+package com.dkss;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int sum=0;
+        int n=17;
+
+        for(n=0; n<=17;n++)
+        {
+            sum = sum+n;
+        }
+
+        System.out.println("Sum of first 17 digits are:"+ sum);
+    }
+}
